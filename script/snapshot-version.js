6a421d51f1e68060f1687875fa2d87c0cf627d66
Effeppi <fran.pes93@gmail.com>
2020-07-01 21:41:04 +0200

    travis configuration

---
+++ script/snapshot-version.js
@@ -0,0 +1,18 @@
+const {execSync} = require('child_process')
+const fs = require('fs')
+
+let version = getVersion()
+
+if(version.endsWith('-SNAPSHOT')) {
+    version = version.concat(`-${(new Date).getTime()}`)
+
+    execSync(`npm version ${version} --git-tag-version=false`)
+
+}
+
+
+function getVersion() {
+    const pkg = fs.readFileSync('./package.json')
+    return JSON.parse(pkg.toString()).version
+
+}
